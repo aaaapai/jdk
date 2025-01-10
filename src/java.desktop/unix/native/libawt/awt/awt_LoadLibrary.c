62db9ef106703314eca9a47e0acf790070e80964
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-01-10 22:37:19 +0800

    Update awt_LoadLibrary.c

--- src/java.desktop/unix/native/libawt/awt/awt_LoadLibrary.c
+++ src/java.desktop/unix/native/libawt/awt/awt_LoadLibrary.c
@@ -107,12 +107,12 @@ AWT_OnLoad(JavaVM *vm, void *reserved)
     char *p, *tk;
     JNI_OnLoad_type *JNI_OnLoad_ptr;
     struct utsname name;
-    JNIEnv *env = (JNIEnv *)JNU_GetEnv(vm, JNI_VERSION_1_2);
+    JNIEnv *env = (JNIEnv *)JNU_GetEnv(vm, JNI_VERSION_25);
     void *v;
 
     if (awtHandle != NULL) {
         /* Avoid several loading attempts */
-        return JNI_VERSION_1_2;
+        return JNI_VERSION_25;
     }
 
     jvm = vm;
@@ -155,7 +155,7 @@ AWT_OnLoad(JavaVM *vm, void *reserved)
         awtHandle = dlopen(buf, RTLD_LAZY | RTLD_GLOBAL);
     }
 
-    return JNI_VERSION_1_2;
+    return JNI_VERSION_25;
 }
 
 JNIEXPORT jint JNICALL
