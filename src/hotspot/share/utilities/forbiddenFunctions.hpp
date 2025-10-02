e11d844d1bf911a064b6991786c1d2b876b4025a
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-02 16:20:00 +0800

    🐒🐒🐒🐒

--- src/hotspot/share/utilities/forbiddenFunctions.hpp
+++ src/hotspot/share/utilities/forbiddenFunctions.hpp
@@ -44,40 +44,4 @@
 #include "forbiddenFunctions_posix.hpp"
 #endif
 
-// Forbid the use of various C library functions.  Some of these have os::
-// replacements that should be used instead.  Others are considered obsolete
-// or have security concerns, either with preferred alternatives, or to be
-// avoided entirely.
-
-FORBID_IMPORTED_NORETURN_C_FUNCTION(void exit(int), , "use os::exit")
-FORBID_IMPORTED_NORETURN_C_FUNCTION(void _Exit(int), , "use os::exit")
-
-// Windows puts _exit in <stdlib.h>, POSIX in <unistd.h>.
-FORBID_IMPORTED_NORETURN_C_FUNCTION(void _exit(int), /* not noexcept */, "use os::exit")
-
-FORBID_IMPORTED_C_FUNCTION(char* strerror(int), , "use os::strerror");
-FORBID_IMPORTED_C_FUNCTION(char* strtok(char*, const char*), , "use strtok_r");
-
-// AIX declarations for sprintf and snprintf are not noexcept, which is
-// inconsistent with most other system header declarations, including being
-// inconsistent with vsprintf and fsnprintf.
-FORBID_C_FUNCTION(int sprintf(char*, const char*, ...), , "use os::snprintf");
-FORBID_C_FUNCTION(int snprintf(char*, size_t, const char*, ...), , "use os::snprintf");
-
-PRAGMA_DIAG_PUSH
-FORBIDDEN_FUNCTION_IGNORE_CLANG_FORTIFY_WARNING
-FORBID_C_FUNCTION(int vsprintf(char*, const char*, va_list), , "use os::vsnprintf");
-FORBID_C_FUNCTION(int vsnprintf(char*, size_t, const char*, va_list), , "use os::vsnprintf");
-PRAGMA_DIAG_POP
-
-// All of the following functions return raw C-heap pointers (sometimes as an
-// option, e.g. realpath or getwd) or, in case of free(), take raw C-heap
-// pointers.  We generally want allocation to be done through NMT.
-FORBID_IMPORTED_C_FUNCTION(void* malloc(size_t size), , "use os::malloc");
-FORBID_IMPORTED_C_FUNCTION(void free(void *ptr), , "use os::free");
-FORBID_IMPORTED_C_FUNCTION(void* calloc(size_t nmemb, size_t size), , "use os::malloc and zero out manually");
-FORBID_IMPORTED_C_FUNCTION(void* realloc(void *ptr, size_t size), , "use os::realloc");
-FORBID_IMPORTED_C_FUNCTION(char* strdup(const char *s), /*noexcept*/, "use os::strdup");
-FORBID_IMPORTED_C_FUNCTION(wchar_t* wcsdup(const wchar_t *s), /*noexcept*/, "don't use");
-
 #endif // SHARE_UTILITIES_FORBIDDENFUNCTIONS_HPP
