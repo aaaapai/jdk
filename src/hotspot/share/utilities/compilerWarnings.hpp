4114ebed23e4f0c825bff716a22444469215f8e1
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-05 19:49:32 +0800

    Update compilerWarnings.hpp

--- src/hotspot/share/utilities/compilerWarnings.hpp
+++ src/hotspot/share/utilities/compilerWarnings.hpp
@@ -29,9 +29,7 @@
 
 #include "utilities/macros.hpp"
 
-#if !defined(__IOS__) && !defined(__ANDROID__)
 #include COMPILER_HEADER(utilities/compilerWarnings)
-#endif
 
 // Defaults when not defined for the TARGET_COMPILER_xxx.
 
