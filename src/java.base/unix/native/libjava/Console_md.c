191edaf319f6b2143a57165c963d43e6f07fb0aa
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-06 16:38:30 +0800

    Update Console_md.c

--- src/java.base/unix/native/libjava/Console_md.c
+++ src/java.base/unix/native/libjava/Console_md.c
@@ -31,8 +31,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+int fileno(FILE* _Nonnull __fp);
 
-__nodiscard int fileno(FILE* _Nonnull __fp);
 
 JNIEXPORT jint JNICALL
 Java_java_io_Console_ttyStatus(JNIEnv *env, jclass cls)
