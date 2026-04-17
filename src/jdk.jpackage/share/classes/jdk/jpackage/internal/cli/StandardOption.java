1fe63a893a260ab28fdf1723487eca988ff8327b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-04-17 19:18:37 +0800

    Update StandardOption.java

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/cli/StandardOption.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/cli/StandardOption.java
@@ -776,6 +776,12 @@ public final class StandardOption {
 
     private static String resourceKeySuffix(OperatingSystem os) {
         switch (os) {
+            case IOS -> {
+                return ".ios";
+            }
+            case ANDROID -> {
+                return ".android";
+            }
             case LINUX -> {
                 return ".linux";
             }
