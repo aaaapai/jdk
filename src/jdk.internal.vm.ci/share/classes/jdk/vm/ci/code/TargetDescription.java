3040e58986c1d881d6c11538495c9a09be4983e9
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-07-12 12:33:24 +0800

    Update TargetDescription.java

--- src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/code/TargetDescription.java
+++ src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/code/TargetDescription.java
@@ -36,8 +36,6 @@ public class TargetDescription {
 
     public final boolean linuxOs = OperatingSystem.isLinux();
     public final boolean macOs = OperatingSystem.isMacOS();
-    public final boolean androidOs = OperatingSystem.isAndroid();
-    public final boolean iosOs = OperatingSystem.isIos();
 
 
     public final Architecture arch;
